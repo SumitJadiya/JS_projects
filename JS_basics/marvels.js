e5b60f3372f73c4a31c295c5820f583e8b9e4b83
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-01-02 15:37:34 +0530

    operations at start, middle and end

--- JS_basics/marvels.js
+++ JS_basics/marvels.js
@@ -3,3 +3,25 @@
 const superHeros = ['IronMan', 'SpiderMan', 1, true, 'Capt. America', 'Thor']
 
 console.log(`we have ${superHeros.length} superHeros`);
+
+const numbers = ['one', 'two', 'three', 'four', 'five', 'six']
+numbers[1] = 'Something'
+console.log(numbers);
+
+// start
+console.log(numbers.shift())
+console.log(numbers);
+
+numbers.unshift("S")
+console.log(numbers);
+
+// end
+console.log(numbers.pop())
+console.log(numbers);
+
+numbers.push('Seven')
+console.log(numbers);
+
+// middle
+console.log(numbers.splice(2,1,'XYZ')); // start from 2 delete 1 element and place XYZ.
+console.log(numbers);
